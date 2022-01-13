fd2c3460989129f3c1a5bd30e138700b92396f22
zakariaelas <zakaria.elas@gmail.com>
2022-01-13 13:57:31 +0100

    add: actionables to both body cards

--- src/components/molecules/actionables.tsx
+++ src/components/molecules/actionables.tsx
@@ -28,8 +28,12 @@ const Actionables: React.FC<ActionablesProps> = ({ actions }) => {
     <div>
       <DropdownMenu.Root>
         <DropdownMenu.Trigger asChild>
-          <Button variant="ghost" size="medium">
-            <MoreHorizontalIcon />
+          <Button
+            className="w-8 h-8 p-1.5 focus:border-none focus:shadow-none"
+            variant="ghost"
+            size="small"
+          >
+            <MoreHorizontalIcon size={20} />
           </Button>
         </DropdownMenu.Trigger>
 
@@ -39,7 +43,7 @@ const Actionables: React.FC<ActionablesProps> = ({ actions }) => {
         >
           {actions.map((action, i) => {
             return (
-              <DropdownMenu.Item key={i}>
+              <DropdownMenu.Item className="mb-1 last:mb-0" key={i}>
                 {
                   <Button
                     variant="ghost"
--- src/domain/settings/return-reasons/index.js
+++ src/domain/settings/return-reasons/index.js
@@ -10,6 +10,9 @@ import BreadCrumb from "../../../components/molecules/breadcrumb"
 import ReturnReasonsList from "./return-reasons-list"
 import BodyCard from "../../../components/organisms/body-card"
 import TwoSplitPane from "../../../components/templates/two-split-pane"
+import PlusIcon from "../../../components/fundamentals/icons/plus-icon"
+import DuplicateIcon from "../../../components/fundamentals/icons/duplicate-icon"
+import TrashIcon from "../../../components/fundamentals/icons/trash-icon"
 
 const ReturnReasons = () => {
   const { return_reasons, isLoading } = useMedusa("returnReasons")
@@ -73,6 +76,16 @@ const ReturnReasons = () => {
       <TwoSplitPane>
         <BodyCard
           title="Return Reasons"
+          actionables={[
+            {
+              label: "Add reason",
+              icon: (
+                <span className="text-grey-90">
+                  <PlusIcon size={20} />
+                </span>
+              ),
+            },
+          ]}
           subtitle="Manage the markets that you will operate within"
         >
           <div className="mt-large">
@@ -88,47 +101,38 @@ const ReturnReasons = () => {
                 </Box>
               </Flex>
             ) : (
-              // <ReturnReasonsList
-              //   return_reasons={showReasons}
-              //   onEditClick={reason => {
-              //     navigate(`/a/settings/return-reasons/${reason.id}`)
-              //   }}
-              // />
               showReasons.map(reason => (
-                <RadioItem
-                  title={reason.label}
-                  value={reason.value}
+                <RadioCard
+                  label={
+                    <>
+                      <span className="inter-base-semibold">
+                        {reason.label}
+                      </span>
+                      <span> (Value: {reason.value})</span>
+                    </>
+                  }
                   description={reason.description}
                   className="mt-xsmall"
                 />
               ))
             )}
-            {/* <Flex width={1} mt={3} justifyContent="space-between">
-          <Text mt={1}>
-          {showReasons.length}{" "}
-            {showReasons.length === 1 ? "reason" : "reasons"}
-          </Text>
-          <Flex alignItems="center">
-            <Button
-              mr={2}
-              variant="primary"
-              onClick={() => onPreviousClick()}
-              disabled={currentPage === 0}
-            >
-            Previous
-            </Button>
-            <Button
-              variant="primary"
-              onClick={() => onNextClick()}
-              disabled={currentPage === numberOfPages - 1}
-              >
-              Next
-              </Button>
-              </Flex>
-            </Flex> */}
           </div>
         </BodyCard>
-        <BodyCard title="Details" subtitle="WRONG_SIZE"></BodyCard>
+        <BodyCard
+          actionables={[
+            {
+              label: "Duplicate reason",
+              icon: <DuplicateIcon size={20} />,
+            },
+            {
+              label: "Delete reason",
+              variant: "danger",
+              icon: <TrashIcon size={20} />,
+            },
+          ]}
+          title="Details"
+          subtitle="WRONG_SIZE"
+        ></BodyCard>
       </TwoSplitPane>
     </div>
   )
@@ -136,21 +140,17 @@ const ReturnReasons = () => {
 
 export default ReturnReasons
 
-const RadioItem = ({ title, subtilte, value, className }) => {
+const RadioCard = ({ label, description }) => {
   return (
     <div className="smthg flex items-baseline bg-grey-0 border-solid border border-grey-20 focus-within:border-violet-60 pointer p-base rounded mt-xsmall">
       <input
         type="radio"
-        className="relative w-[20px] h-[20px] border-solid rounded-circle border border-grey-30 checked:border-violet-60 checked:border-2 after:bg-violet-60 after:w-[12px] after:h-[12px] after:rounded-circle after:m-auto after:inset-0 after:absolute after:invisible checked:after:visible"
+        // extremely long classNames .. but we can extract this into global.css and make it a component
+        className="appearance-none relative w-[20px] h-[20px] border-solid rounded-circle border border-grey-30 checked:border-violet-60 checked:border-2 after:bg-violet-60 after:w-[12px] after:h-[12px] after:rounded-circle after:m-auto after:inset-0 after:absolute after:invisible checked:after:visible"
       />
       <div className="ml-small">
-        <div className="-mt-[2px]">
-          <span className="inter-base-semibold">Wrong Size</span>
-          <span>(Value: WRONG_SIZE)</span>
-        </div>
-        <p className="inter-small-regular text-grey-50">
-          Customer wishes to replace the item due to a wrong size.
-        </p>
+        <div className="-mt-[2px]">{label}</div>
+        <p className="inter-small-regular text-grey-50">{description}</p>
       </div>
     </div>
   )
